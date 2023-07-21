cce92162e5e5a4d4b250a2a629d789abb4eabb5f
levimaciell <levimaciell1221@gmail.com>
2023-07-21 10:21:51 -0300

    properties, game, orm

--- src/main/java/com/example/dslist/entities/Game.java
+++ src/main/java/com/example/dslist/entities/Game.java
@@ -1,9 +1,22 @@
 package com.example.dslist.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_GAMES")
 public class Game {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
@@ -99,6 +112,31 @@ public class Game {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Game other = (Game) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     
